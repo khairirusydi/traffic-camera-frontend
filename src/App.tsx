f4601425d3e4e7756b9a936c7b36387cf55bced2
Khairi Rusydi <krusydi@palo-it.com>
2024-03-20 10:06:52 +0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Heading } from '@chakra-ui/react';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Heading>Traffic Camera</Heading>
+    </>
+  );
+}
+
+export default App;
